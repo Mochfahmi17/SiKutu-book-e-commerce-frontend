73d955cae21d83133bda7d80ba8a67d21541380c
Mochfahmi17 <mochammadfahmiks@gmail.com>
2025-11-29 14:27:22 +0700

    change background in news letter input

--- src/components/footer/FooterNewsLetter.tsx
+++ src/components/footer/FooterNewsLetter.tsx
@@ -8,7 +8,7 @@ const FooterNewsLetter = () => {
         <input
           type="email"
           placeholder="Email"
-          className="w-full bg-gray-400 py-2 pr-12 pl-3 text-sm text-white shadow-sm placeholder:text-gray-300 focus-visible:outline-none disabled:opacity-50"
+          className="w-full bg-gray-500 py-2 pr-12 pl-3 text-sm text-white shadow-sm placeholder:text-gray-300 focus-visible:outline-none disabled:opacity-50"
         />
         <button className="absolute right-0 h-full bg-white px-2 text-gray-800">
           <IoIosSend className="size-6" />
